14e40f0ec154266a773854ecf37b6bfaccbf6091
Grzegorz Jewusiak <grzegorz@jewusiak.pl>
2023-11-02 20:54:29 +0100

    Added base URL dependency on .properties file

--- src/main/java/pl/jewusiak/todo_list/TodoListApplication.java
+++ src/main/java/pl/jewusiak/todo_list/TodoListApplication.java
@@ -1,12 +1,18 @@
 package pl.jewusiak.todo_list;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.extensions.Extension;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Todo List API", version = "1.0", description = "Documentation Todo List API v1.0"))
+@OpenAPIDefinition(info = @Info(title = "Todo List API",
+                                version = "1.0",
+                                description = "Documentation Todo List API v1.0"),
+                   servers = @Server(url = "${pl.jewusiak.todo_list.swagger.server.url}"))
 public class TodoListApplication {
 
     public static void main(String[] args) {
